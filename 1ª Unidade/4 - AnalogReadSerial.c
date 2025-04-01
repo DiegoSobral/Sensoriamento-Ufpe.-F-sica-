ad5c21e718cf1bd15cf9429a96fdfeff3cdf0c93
Diego Ribeiro da Silva Sobral <114121416+DiegoSobral@users.noreply.github.com>
2025-04-01 11:15:28 -0300

    Update 4 - AnalogReadSerial.c

--- 1ª Unidade/4 - AnalogReadSerial.c
+++ 1ª Unidade/4 - AnalogReadSerial.c
@@ -1,11 +1,11 @@
 void setup() {
-Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-// read the input on analog pin 0:
-int sensorValue = analogRead(A0);
-// print out the value you read:
-Serial.println(sensorValue);
-delay(1); // delay in between reads for stability
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(1); // delay in between reads for stability
 }
