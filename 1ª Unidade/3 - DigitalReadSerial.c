7f8ce5c79db85c2a87584728b1eda8657331baf4
Diego Ribeiro da Silva Sobral <114121416+DiegoSobral@users.noreply.github.com>
2025-04-01 11:16:12 -0300

    Update 3 - DigitalReadSerial.c

--- 1ª Unidade/3 - DigitalReadSerial.c
+++ 1ª Unidade/3 - DigitalReadSerial.c
@@ -2,16 +2,16 @@
 int pushButton = 2;
 
 void setup() {
-// initialize serial communication at 9600 bits per second:
-Serial.begin(9600);
-// make the pushbutton's pin an input:
-pinMode(pushButton, INPUT);
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // make the pushbutton's pin an input:
+  pinMode(pushButton, INPUT);
 }
 
 void loop() {
-// read the input pin:
-int buttonState = digitalRead(pushButton);
-// print out the state of the button:
-Serial.println(buttonState);
-delay(1); // delay in between reads for stability
+  // read the input pin:
+  int buttonState = digitalRead(pushButton);
+  // print out the state of the button:
+  Serial.println(buttonState);
+  delay(1); // delay in between reads for stability
 }
